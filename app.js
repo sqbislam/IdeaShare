26284eae17377382792ebe8afe27f279ac0a7297
Saqib Islam <sqbislam101@gmail.com>
2019-02-15 16:00:23 +0600

    Initial Commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-app.js
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app  = express();
+const port = process.env.port || 5000;
+
+app.listen(port, ()=> {
+	
+	console.log(`Server started on port ${port}`);
+
+})
+
+app.get('/', (req, res) => {
+
+	res.send('It works!!')
+})
